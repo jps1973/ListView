260062bae77c589f85464f3268269597143bc830
jps1973 <47426983+jps1973@users.noreply.github.com>
2024-11-30 14:50:02 +0000

    Removed List Box Window files

--- make.bat
+++ make.bat
@@ -4,4 +4,4 @@ del *.exe
 
 windres Resource.rc -o Resource.o
 
-g++ -mwindows -m64 -static -Wall -Wextra ListView.cpp ListBoxWindow.cpp ListViewWindow.cpp StatusBarWindow.cpp Resource.o -o ListView.exe
+g++ -mwindows -m64 -static -Wall -Wextra ListView.cpp ListViewWindow.cpp StatusBarWindow.cpp Resource.o -o ListView.exe
